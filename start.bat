7e659f636aa83169bcb931ab718696dffe1e5a45
AkalSpring <bidya@akalspring.com>
2022-05-29 17:53:54 +0530

    added new bill

--- start.bat
+++ start.bat
@@ -3,7 +3,6 @@ echo WELCOME TO AKALTOOLS
 git add . 
 git commit -m "added new bill"
 git push 
-python manage.py runserver
 "C:\Users\THEUSER\AppData\Local\Google\Chrome\Application\chrome.exe"
-sleep1
 start "Akal Tools" "http://127.0.0.1:8000/"
+python manage.py runserver
