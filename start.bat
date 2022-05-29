7cbd7acd1cca7d48bb9204e8c7483874e6e1ba9f
Prince Mishra <prince.mishra1384@gmail.com>
2022-05-29 16:22:04 +0530

    added new bill

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
-start cmd /k "python manage.py runserver"
-@echo off
-"C:\Users\THEUSER\AppData\Local\Google\Chrome\Application\chrome.exe"
-sleep1
-start "Akal Tools" "http://127.0.0.1:8000/"
+git add . 
+git commit -m "added new bill"
+git push
+pause
